58644c4e6eb65536109316e0c7b60b39a6417711
Megan Reddy <meganr28@seas.upenn.edu>
2023-01-14 18:54:17 -0500

    Adding prefabs and text support

--- Roids/Assets/Scripts/Asteroid.cs
+++ Roids/Assets/Scripts/Asteroid.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    public GameObject deathExplosion;
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointValue = 10;
     }
 
     // Update is called once per frame
@@ -18,8 +21,11 @@ public class Asteroid : MonoBehaviour
 
     public void Die()
     {
-        // Destroy removes the gameObject from the scene and
-        // marks it for garbage collection
+        Instantiate(deathExplosion, gameObject.transform.position,
+        Quaternion.AngleAxis(-90, Vector3.right));
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
         Destroy(gameObject);
     }
 }
