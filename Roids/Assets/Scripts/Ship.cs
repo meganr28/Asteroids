3f03a5cc39003103626e4907dc7c88a218d98189
Megan Reddy <meganr28@seas.upenn.edu>
2023-01-14 11:13:23 -0500

    Rigid body component

--- Roids/Assets/Scripts/Ship.cs
+++ Roids/Assets/Scripts/Ship.cs
@@ -5,29 +5,45 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     // public variables to tune Ship's movement
-    public float turnSpeed;
-    public float thrustSpeed;
+    public Vector3 forceVector;
+    public float rotationSpeed;
+    public float rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        turnSpeed = .5f; thrustSpeed = .01f;
+        // Vector3 default initializes all components to 0.0f
+        forceVector.x = 1.0f;
+        rotationSpeed = 2.0f;
+    }
+
+    /* forced changes to rigid body physics parameters should be done through the FixedUpdate()
+    method, not the Update() method
+    */
+    void FixedUpdate()
+    {
+        // force thruster
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(forceVector);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rotation += rotationSpeed;
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rotation -= rotationSpeed; 
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            gameObject.transform.Translate(0, 0, thrustSpeed);
-        }
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            gameObject.transform.Rotate(0, turnSpeed, 0);
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            gameObject.transform.Rotate(0, -turnSpeed, 0);
-        }
+
     }
 }
